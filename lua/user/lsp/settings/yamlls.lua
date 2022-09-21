717d9161cd27557e70a575b0916ab60315287353
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-09-22 01:44:16 +0500

    k8s support yamls

--- lua/user/lsp/settings/yamlls.lua
+++ lua/user/lsp/settings/yamlls.lua
@@ -1,11 +1,22 @@
 return {
-  yaml = {
-    schemaStore = {
-      enable = true,
-    },
-    schemas = {
-      ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+  -- filetypes = {"yaml"},
+  settings = {
+    yaml = {
+      format = {
+        enable = true,
+      },
+      validate = true,
+      schemas = {
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yml",
+        ["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.yaml",
+        kubernetes = "*.yaml",
+      },
+      -- scheme = {
+      --   ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yml",
+      --   ["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.yaml",
+      --   ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.20.5-standalone-strict/all.json"] = "frontend-service.yaml"
+      -- },
+      schemaDownload = { enable = true },
     },
   },
-  filetypes = { "yaml", "yaml.docker-compose" },
 }
