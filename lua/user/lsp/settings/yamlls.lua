1308346f6f3412045745648f216386f472d63bfb
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-10-28 21:44:40 +0500

    yaml updated & github workflows added to yaml

--- lua/user/lsp/settings/yamlls.lua
+++ lua/user/lsp/settings/yamlls.lua
@@ -1,5 +1,4 @@
 return {
-  -- filetypes = {"yaml"},
   settings = {
     yaml = {
       format = {
@@ -7,6 +6,7 @@ return {
       },
       validate = true,
       schemas = {
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
         ["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/master/docs/content/en/schemas/v2beta8.json"] = "skaffold.yaml",
         ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yml",
         ["https://json.schemastore.org/pre-commit-config.json"] = ".pre-commit-config.yaml",
